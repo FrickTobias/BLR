43fd906379fc092b9bac88e4491b1af6360298f8
Majsstenen <hans.tobias.frick@gmail.com>
2017-10-23 10:49:19 +0200

    bugfix. Forgot instance in arg.

--- setpath.sh
+++ setpath.sh
@@ -1,12 +1,10 @@
 #! /bin/bash
 
-# Creates paths.sh file which can be run in another shell using '. paths.sh' to fetch the variables defined in the paths.sh script.
+# Creates paths.txt file which can be run in another shell using '. paths.sh' to fetch the variables defined in the paths.sh script.
 
-# bash setpath.sh <wgh_path> <picard_path> <bowtie2_reference> <fragScaff_path>
+# bash setpath.txt <wgh_path> <picard_path> <bowtie2_reference> <fragScaff_path>
 
-printf '#! /bin/bash/\n' > paths.sh
-
-printf '\nWGH_path='$1 >> paths.sh
-printf '\npicard_path='$2 >> paths.sh
-printf '\nbowtie2_reference='$3 >> paths.sh
-printf '\nfragScaff_path='$4 >> paths.sh
+printf 'WGH_path='$1 > paths.txt
+printf '\npicard_path='$2 >> paths.txt
+printf '\nbowtie2_reference='$3 >> paths.txt
+printf '\nfragScaff_path='$4 >> paths.txt
