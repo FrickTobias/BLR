474cfa76da810e5b256d3a64f25fe85e4f994a0e
Majsstenen <hans.tobias.frick@gmail.com>
2017-10-25 10:03:43 +0200

    Adding mail to automation test for 'Main pipe' according to old pic

--- bash scripts/WGH_read_processing.sh
+++ bash scripts/WGH_read_processing.sh
@@ -26,6 +26,7 @@ file_name2="$path/${name_ext2%.*}"
 mkdir -p $path &&
 
 printf '#1 START PROCESSING \n' &&
+echo 'Starting' | mail -s 'wgh' tobias.frick@scilifelab.se &&
 
 
 # Trim away E handle on R1 5'.
@@ -36,6 +37,7 @@ cutadapt -g CAGTTGATCATCAGCAGGTAATCTGG \
 
 
 printf '\n\n#2 TRIMMED E \n' &&
+echo 'first handle' | mail -s 'wgh' tobias.frick@scilifelab.se &&
 
 
 # Get DBS using UMI-Tools -> _BDHVBDVHBDVHBDVH in header.
@@ -48,6 +50,7 @@ umi_tools extract --stdin=$file_name".e_removed.fastq" \
 
 
 printf '\n\n#3 GOT DBS USING UMI-TOOLs \n' &&
+echo 'bc extracted' | mail -s 'wgh' tobias.frick@scilifelab.se &&
 
 
 #Cut TES from 5' of R1. TES=AGATGTGTATAAGAGACAG. Discard untrimmed.
@@ -58,6 +61,7 @@ cutadapt -g AGATGTGTATAAGAGACAG -o $file_name".TES1_removed.fastq" \
 
 
 printf '\n\n#4 TRIMMED TES1 \n' &&
+echo 'Second handle' | mail -s 'wgh' tobias.frick@scilifelab.se &&
 
 
 #Cut TES' from 3' for R1 and R2. TES'=CTGTCTCTTATACACATCT
@@ -68,11 +72,13 @@ cutadapt -a CTGTCTCTTATACACATCT -A CTGTCTCTTATACACATCT \
 	$file_name2".TES1_removed.fastq" -e 0.2 &&
 
 printf '\n\n#5 TRIMMED TES2 \n' &&
+echo 'Trimmed 3prim' | mail -s 'wgh' tobias.frick@scilifelab.se &&
 
-bowtie2 --maxins 2000 -x /Users/pontushojer/data_analysis/Reference/Bowtie2Index/genome \
+bowtie2 --maxins 2000 -x /shared/reference/Bowtie2Index/genome \
     -1 $1 -2 $2 -S $path/"mappedInserts.sam" | samtools view -bh - > $path/"mappedInserts.bam" &&
 
 printf '\n\n#6 MAPPED READS \n' &&
+echo 'Mapped' | mail -s 'wgh' tobias.frick@scilifelab.se &&
 
 rm $path/"mappedInserts.sam" &&
 
@@ -81,7 +87,8 @@ printf '\n\n#7 REMOVED SAM-FILE \n' &&
 samtools sort $path/"mappedInserts.bam" \
     -o $path/"mappedInserts.sort.bam" &&
 
-samtools index $path/"mappedInserts.sort.bam" &&
+echo 'Sorted' | mail -s 'wgh' tobias.frick@scilifelab.se &&
+#samtools index $path/"mappedInserts.sort.bam" &&
 
 printf '\n\n#8 SORTED AND INDEXED BAM-FILE \n' &&
 
