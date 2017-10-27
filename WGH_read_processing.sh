0102ffb299f93219fc851990b66ca3925539c25e
Majsstenen <hans.tobias.frick@gmail.com>
2017-10-27 12:07:28 +0200

    Removing &&

--- WGH_read_processing.sh
+++ WGH_read_processing.sh
@@ -37,7 +37,7 @@ file_name2="$path/${name_ext2%.*}"
 
 mkdir -p $path
 
-echo 'Starting 1st trim '$(date) | mail -s 'wgh' tobias.frick@scilifelab.se &&
+echo 'Starting 1st trim '$(date) | mail -s 'wgh' tobias.frick@scilifelab.se
 printf '#1 START PROCESSING \n'
 
 # Trim away E handle on R1 5'. Also removes reads shorter than 85 bp.
@@ -46,7 +46,7 @@ cutadapt -g ^CAGTTGATCATCAGCAGGTAATCTGG \
     -p $file_name2".h1.fastq" $1 $2 \
     --discard-untrimmed -e 0.2 -m 65 # Tosses reads shorter than len(e+bc+handle+TES)
 
-echo 'Starting umi extraction '$(date) | mail -s 'wgh' tobias.frick@scilifelab.se &&
+echo 'Starting umi extraction '$(date) | mail -s 'wgh' tobias.frick@scilifelab.se
 printf '\n\n#2 TRIMMED E \n'
 
 # Get DBS using UMI-Tools -> _BDHVBDVHBDVHBDVH in header.
@@ -60,7 +60,7 @@ umi_tools extract --stdin=$file_name".h1.fastq" \
 rm $file_name".h1.fastq"
 rm $file_name2".h1.fastq"
 
-echo 'Starting 2nd trim '$(date) | mail -s 'wgh' tobias.frick@scilifelab.se &&
+echo 'Starting 2nd trim '$(date) | mail -s 'wgh' tobias.frick@scilifelab.se
 printf '\n\n#3 GOT DBS USING UMI-TOOLs \n'
 
 #Cut TES from 5' of R1. TES=AGATGTGTATAAGAGACAG. Discard untrimmed.
@@ -72,7 +72,7 @@ cutadapt -g AGATGTGTATAAGAGACAG -o $file_name".h1.bc.h2.fastq" \
 rm $file_name".h1.bc.fastq"
 rm $file_name2".h1.bc.fastq"
 
-echo 'Starting 3rd trim (final) '$(date) | mail -s 'wgh' tobias.frick@scilifelab.se &&
+echo 'Starting 3rd trim (final) '$(date) | mail -s 'wgh' tobias.frick@scilifelab.se
 printf '\n\n#4 TRIMMED TES1 \n'
 
 #Cut TES' from 3' for R1 and R2. TES'=CTGTCTCTTATACACATCT
@@ -86,7 +86,7 @@ cutadapt -a CTGTCTCTTATACACATCT -A CTGTCTCTTATACACATCT \
 rm $file_name".h1.bc.h2.fastq"
 rm $file_name2".h1.bc.h2.fastq"
 
-echo 'Starting mapping '$(date) | mail -s 'wgh' tobias.frick@scilifelab.se &&
+echo 'Starting mapping '$(date) | mail -s 'wgh' tobias.frick@scilifelab.se
 printf '\n\n#5 TRIMMED TES2 \n'
 
 bowtie2 --maxins 2000 -x $bowtie2_reference \
@@ -97,7 +97,7 @@ samtools view -bh $path/"mappedInserts.sam" > $path/"mappedInserts.bam"
 pigz $file_name".trimmed.fastq"
 pigz $file_name2".trimmed.fastq"
 
-echo 'Starting bam file sorting '$(date) | mail -s 'wgh' tobias.frick@scilifelab.se &&
+echo 'Starting bam file sorting '$(date) | mail -s 'wgh' tobias.frick@scilifelab.se
 printf '\n\n#6 MAPPED READS \n'
 
 rm $path/"mappedInserts.sam"
@@ -111,7 +111,7 @@ rm $path/"mappedInserts.bam"
 
 samtools index $path/"mappedInserts.sort.bam"
 
-echo 'Finished '$(date) | mail -s 'wgh' tobias.frick@scilifelab.se &&
+echo 'Finished '$(date) | mail -s 'wgh' tobias.frick@scilifelab.se
 printf '\n\n#8 SORTED AND INDEXED BAM-FILE \n'
 
 printf 'RUN COMPLETE (>'-')  (>'-')>  ^('-')^'
--- clustering.sh
+++ clustering.sh
@@ -13,18 +13,18 @@ r1 = $1
 bam = $2
 output = $3
 
-echo 'Starting bc extraction '$(date) | mail -s 'wgh' tobias.frick@scilifelab.se &&
+echo 'Starting bc extraction '$(date) | mail -s 'wgh' tobias.frick@scilifelab.se
 python3 $wgh_path/python_scripts/cdhit_prep.py $r1 $output/unique_bc -r 3 -f 0
 
-echo 'Starting clustering '$(date) | mail -s 'wgh' tobias.frick@scilifelab.se &&
+echo 'Starting clustering '$(date) | mail -s 'wgh' tobias.frick@scilifelab.se
 for file in $output/unique_bc/*;
     do;
     cdhit-454 -i $file -o $file'.clustered' -T 0 -c 0.9 -gap -100 -g 1 -n 3 -M 0;
     done;
 
-echo 'Starting tagging '$(date) | mail -s 'wgh' tobias.frick@scilifelab.se &&
+echo 'Starting tagging '$(date) | mail -s 'wgh' tobias.frick@scilifelab.se
 cat $output/unique_bc/*.clstr > $output/NNN.clstr
 
 python3 $wgh_path/python_scripts/tag_bam.py $output/NNN.clstr $bam $bam'.tagged.bam'
 
-echo 'Finished '$(date) | mail -s 'wgh' tobias.frick@scilifelab.se &&
+echo 'Finished '$(date) | mail -s 'wgh' tobias.frick@scilifelab.se
