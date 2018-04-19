ea0c22ba9310efb2c38506c173fe701a8f2541ad
tobiasfrick <hans.tobias.frick@gmail.com>
2018-04-19 09:04:59 +0200

    Bugfix: faulty syntax, unpaired reads positions

--- python scripts/cluster_rmdup.py
+++ python scripts/cluster_rmdup.py
@@ -124,13 +124,12 @@ def main():
         positions = unpaired_read.get_reference_positions()
 
         # Handles unmapped unpaired reads
-        try: start_stop[0]
+        try: positions[0]
         except IndexError:
             summaryInstance.unmapped_singleton += 1
             continue
 
         start_stop = (positions[0], positions[-1])
-
 
         # Add chrom to dict if not present
         if not chromosome in unpaired_duplicate_tracker:
