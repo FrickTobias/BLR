88f734e1307faae19d6c6b30493d15a1ceb8e2b9
tobiasfrick <hans.tobias.frick@gmail.com>
2018-10-03 16:53:20 +0200

    Updated & fully incorporated up-to-date std class scripts

--- python scripts/bc_extract.py
+++ python scripts/bc_extract.py
@@ -17,54 +17,36 @@ def main():
     # Process data
     #
 
-    report_progress('Fetching a 20 bp barcode from sequence in r1')
-    counter = int()
-    totalcounter = int()
-    limit = 1000000
-    # Reads both files at the same time in order to get bc seq into r2 as well
-    with open(args.r1) as f1, open(args.r2) as f2:
-        with open(args.out_r1, 'w') as openr1, open(args.out_r2, 'w') as openr2:
-            for x, y in zip(f1, f2):
-                counter += 1
-                # Fetch an entire read pair before parsing
-                if counter == 1:
-                    if not x.split()[0] == y.split()[0]:
-                        sys.stderr.write('Headers does not match!')
-                        sys.exit()
-                    else:
-                        header_r1 = x
-                        header_r2 = y
-                elif counter == 2:
-                    seq_r1 = x
-                    seq_r2 = y
-                elif counter == 3:
-                    comment_r1 = x
-                    comment_r2 = y
-                elif counter == 4:
-                    qual_r1 = x
-                    qual_r2 = y
+    report_progress('Starting')
+    progress = ProgressReporter('Read pairs processed', 1000000)
+    generator = FileReader(args.r1, args.r2)
+    with open(args.out_r1, 'w') as openr1, open(args.out_r2, 'w') as openr2:
+        for read1, read2 in generator.fastqPairedReader():
 
-                    # Reset
-                    counter = 0
-                    totalcounter += 1
+            # Adjusting for BC
+            bc_seq = read1.seq[:20]
+            read1.seq = read1.seq[20:]
+            read1.qual = read1.qual[20:]
 
-                    # Readpair fetched, parsing
-                    bc_seq = seq_r1[:20]
-                    seq_r1_trimmed = seq_r1[20:]
-                    qual_r1_trimmed = qual_r1[20:]
+            # Header parsing
+            name_and_pos_r1 = read1.header.split()[0]
+            read_and_index_r1 = read1.header.split()[1]
+            name_and_pos_r2 = read2.header.split()[0]
+            read_and_index_r2 = read2.header.split()[1]
 
-                    name_and_pos_r1 = header_r1.split()[0]
-                    read_and_index_r1 = header_r1.split()[1]
+            # Save header to read instances
+            read1.header = name_and_pos_r1 + '_' + bc_seq + ' ' + read_and_index_r1
+            read2.header = name_and_pos_r2 + '_' + bc_seq + ' ' + read_and_index_r2
 
-                    name_and_pos_r2 = header_r2.split()[0]
-                    read_and_index_r2 = header_r2.split()[1]
+            # Write to out
+            openr1.write(read1.fastq_string())
+            openr2.write(read2.fastq_string())
 
-                    openr1.write(name_and_pos_r1 + '_' + bc_seq + ' ' + read_and_index_r1 + '\n' + seq_r1_trimmed + comment_r1 + qual_r1_trimmed)
-                    openr2.write(name_and_pos_r2 + '_' + bc_seq + ' ' + read_and_index_r2 + '\n' + seq_r2 + comment_r2 + qual_r2)
+            # Progress reporting
+            progress.update()
 
-                    if totalcounter >= limit:
-                        limit += 1000000
-                        report_progress(str(totalcounter) + '\treads processed')
+    generator.close()
+    report_progress('Finished')
 
 def report_progress(string):
     """
@@ -73,6 +55,25 @@ def report_progress(string):
     Output: [date]  string
     """
     sys.stderr.write(time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime()) + '\t' + string + '\n')
+
+class ProgressReporter(object):
+    """
+    Writes to out during iteration of unknown length
+    """
+
+    def __init__(self, name_of_process, report_step):
+
+        self.name = name_of_process
+        self.report_step = report_step
+        self.position = int()
+        self.next_limit = report_step
+
+    def update(self):
+
+        self.position += 1
+        if self.position >= self.next_limit:
+            report_progress(self.name + '\t' + "{:,}".format(self.position))
+            self.next_limit += self.report_step
 
 class ProgressBar(object):
     """
@@ -114,6 +115,113 @@ class ProgressBar(object):
 
     def terminate(self):
          sys.stderr.write('\n')
+
+class FileReader(object):
+    """
+    Reads input files, handles gzip.
+    """
+    def __init__(self, filehandle, filehandle2=None):
+
+        # Init variables setting
+        self.filehandle = filehandle
+        self.gzip = bool()
+
+        # Open files as zipped or not not (depending on if they end with .gz)
+        if self.filehandle[-3:] == '.gz':
+            report_progress('File detected as gzipped, unzipping when reading')
+            import gzip
+            self.openfile = gzip.open(self.filehandle, 'r')
+            self.gzip = True
+        else:
+            self.openfile = open(self.filehandle, 'r')
+
+        # Paired end preparation
+        self.filehandle2 = filehandle2
+        if self.filehandle2:
+
+            # Open files as zipped or not not (depending on if they end with .gz)
+            if self.filehandle2[-3:] == '.gz':
+                report_progress('File detected as gzipped, unzipping when reading')
+                import gzip
+                self.openfile2 = gzip.open(self.filehandle2, 'r')
+            else:
+                self.openfile2 = open(self.filehandle2, 'r')
+
+    def fileReader(self):
+        """
+        Reads non-specific files as generator
+        :return: lines
+        """
+        for line in self.openfile:
+            if self.gzip:
+                line = line.decode("utf-8")
+            yield line
+
+    def fastqReader(self):
+        """
+        Reads lines 4 at the time as generator
+        :return: read as fastq object
+        """
+
+        line_chunk = list()
+        for line in self.openfile:
+            if self.gzip:
+                line = line.decode("utf-8")
+            line_chunk.append(line)
+            if len(line_chunk) == 4:
+                read = FastqRead(line_chunk)
+                line_chunk = list()
+                yield read
+
+    def fastqPairedReader(self):
+        """
+        Reads two paired fastq files and returns a pair of two reads
+        :return: read1 read2 as fastq read objects
+        """
+
+        line_chunk1 = list()
+        line_chunk2 = list()
+        for line1, line2 in zip(self.openfile, self.openfile2):
+            if self.gzip:
+                line1 = line1.decode("utf-8")
+                line2 = line2.decode("utf-8")
+            line_chunk1.append(line1)
+            line_chunk2.append(line2)
+            if len(line_chunk1) == 4 and len(line_chunk2) == 4:
+                read1 = FastqRead(line_chunk1)
+                read2 = FastqRead(line_chunk2)
+
+                # Error handling
+                if not read1.header.split()[0] == read2.header.split()[0]:
+                    import sys
+                    sys.exit('INPUT ERROR: Paired reads headers does not match.\nINPUT ERROR: Read pair number:\t'+str(progress.position+1)+'\nINPUT ERROR: '+str(read1.header)+'\nINPUT ERROR: '+str(read2.header)+'\nINPUT ERROR: Exiting')
+                line_chunk1 = list()
+                line_chunk2 = list()
+                yield read1, read2
+
+    def close(self):
+        """
+        Closes files properly so they can be re-read if need be.
+        :return:
+        """
+        self.openfile.close()
+        if self.filehandle2:
+            self.openfile2.close()
+
+class FastqRead(object):
+    """
+    Stores read as object.
+    """
+
+    def __init__(self, fastq_as_line):
+
+        self.header = fastq_as_line[0].strip()
+        self.seq = fastq_as_line[1].strip()
+        self.comment = fastq_as_line[2].strip()
+        self.qual = fastq_as_line[3].strip()
+
+    def fastq_string(self):
+        return self.header + '\n' + self.seq  + '\n' + self.comment  + '\n' + self.qual + '\n'
 
 class readArgs(object):
     """
