51db29fd5025df4b23a1edcf2dd467df50c05244
Marcel Martin <marcel.martin@scilifelab.se>
2019-06-05 10:23:33 +0200

    Configure logging
    Instead of printing stuff with print(), it is a good idea to use a proper
    logging infrastructure, such as Python’s logging module that is in the
    stdlib. With this, messages can be of different levels (debug, info, warning,
    error) and it’s possible to dynamically configure which ones are shown and
    where they are written. For example, all logging output could be sent to a
    file, but warning-level messages and higher should also be printed to the
    screen.

--- src/blr/__main__.py
+++ src/blr/__main__.py
@@ -1,11 +1,17 @@
 import sys
+import logging
+
+
+logger = logging.getLogger(__name__)
 
 
 def main() -> int:
-    print("Hello")
+    logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
+    logger.warning("A warning!")
+    logger.info("Hello")
+
     return 0
 
 
 if __name__ == "__main__":
     sys.exit(main())
-
