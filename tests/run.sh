0dc5f52fff1e7f4619205071edd77d3f2ffbe10d
tobiasfrick <hans.tobias.frick@gmail.com>
2019-06-17 11:40:40 +0200

    Change md5 back to md5sum

--- tests/run.sh
+++ tests/run.sh
@@ -5,5 +5,5 @@ set -xeuo pipefail
 
 rm -rf outdir
 bash BLR_automation.sh testdata/reads.1.fastq.gz testdata/reads.2.fastq.gz outdir
-m=$(samtools view outdir/reads.1.fastq.sort.tag.rmdup.x2.filt.bam | md5 | cut -f1 -d" ")
+m=$(samtools view outdir/reads.1.fastq.sort.tag.rmdup.x2.filt.bam | md5sum | cut -f1 -d" ")
 test $m == 51788863e44c1a43f8da7287b1cb2867
