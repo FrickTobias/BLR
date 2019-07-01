e3796737ae3320151667b70aabdd654fd524769f
Pontus Hojer <pontushojer@gmail.com>
2019-07-01 20:03:00 +0200

    Updates following PR review + extra fixes - Extended script description - Removed function for checking input/output. Will rely on argparse descriptions of input and requirements. - Remove option to select interleaved files. This will instead be inferred from input/output based on simple logic: if file1 and not file2 -> interleaved.

--- src/blr/cli/extractbarcode.py
+++ src/blr/cli/extractbarcode.py
@@ -1,6 +1,7 @@
 """
 Extract barcode sequences by moving 20 bp from 5' end of the read to the header,
-separated by an underline.
+separated by an underline. Only accepts paired-end input (interleaved or not).
+Script can handle gzipped files (.gz).
 """
 import logging
 import sys
@@ -11,66 +12,28 @@ import blr.utils as BLR
 logger = logging.getLogger(__name__)
 
 
-def input_files(inputs, interleaved):
-    """
-    Checks input arguments to see if they are sound and assigns them to input1 and input2
-    :param inputs: list of strings. Input files or paths given.
-    :param interleaved: boolean. If input is interleaved or not.
-    :return:
-    """
-    input1, input2 = (None, None)
-    if len(inputs) == 0:
-        sys.exit(f'EXITING: No inputs given!')
-    elif len(inputs) > 2:
-        sys.exit(f'EXITING: Too many files given as input {" ".join(inputs)}')
-    elif len(inputs) == 2:
-        input1, input2 = inputs
-        if interleaved:
-            interleaved = False
-        logger.info(f'Extracting barocodes from fastq {input1} and {input2}')
-    elif len(inputs) == 1 and interleaved:
-        input1 = inputs[0]
-        logger.info(f'Extracting barocodes from interleaved fastq {input1}')
-    else:
-        sys.exit(f'EXITING: Files given as input ({inputs}) does not match the required 1 or 2.')
-
-    return input1, input2, interleaved
-
-
-def output_files(output1, output2):
-    """
-    Checks output arguments given and if file should be written as interleaved or not.
-    :param output1: string. Output1 argument
-    :param output2: string. Output2 argument
-    :return: output1, output2, interleaved.
-    """
-    interleaved = bool()
-    if output1 and output2:
-        interleaved = False
-        logger.info(f'Writing paired fastq to files {output1} and {output2}')
-    elif not output1 and not output2:
-        output1 = sys.stdout.buffer
-        interleaved = True
-        logger.info(f'Writing interleaved fastq to stdout.')
-    elif output1 and not output2:
-        interleaved = True
-        logger.info(f'Writing interleaved fastq to file {output1}.')
-
-    return output1, output2, interleaved
-
-
 def main(args):
     logger.info(f'Starting')
 
     progress = BLR.ProgressReporter('Read pairs processed', 1000000)
 
-    inputfile1, inputfile2, input_interleaved = input_files(args.input, args.interleaved)
+    input_interleaved = True if not args.input2 else False
 
-    reader = dnaio.open(inputfile1, file2=inputfile2, interleaved=input_interleaved, mode="r", fileformat="fastq")
+    logger.info(f"Input detected as {'interleaved fastq.' if input_interleaved else 'paired fastq.'}")
 
-    outputfile1, outputfile2, output_interleaved = output_files(args.output1, args.output2)
+    # If no output1 is given output is sent to stdout
+    if not args.output1:
+        logger.info(f"Writing output to stdout.")
+        args.output1 = sys.stdout.buffer
+        args.output2 = None
 
-    writer = dnaio.open(outputfile1, file2=outputfile2, interleaved=output_interleaved, mode="w", fileformat="fastq")
+    output_interleaved = True if not args.output2 else False
+
+    logger.info(f"Output detected as {'interleaved fastq.' if output_interleaved else 'paired fastq.'}")
+
+    reader = dnaio.open(args.input1, file2=args.input2, interleaved=input_interleaved, mode="r", fileformat="fastq")
+
+    writer = dnaio.open(args.output1, file2=args.output2, interleaved=output_interleaved, mode="w", fileformat="fastq")
 
     for read1, read2 in reader:
         # Adjusting for BC
@@ -98,15 +61,16 @@ def main(args):
 
 
 def add_arguments(parser):
-    # Files
-    parser.add_argument("input", nargs='*',
-                        help="Input paired fastq for read1 and read2. Use '-' if using stdin.")
-    parser.add_argument("-o1", "--output1", default=None,
-                        help="Output file name for read1. If not specified the result is written to stdout"
-                             " as interleaved fastq.")
-    parser.add_argument("-o2", "--output2", default=None,
-                        help="Output file name for read2. If not specified but -o1 given the result is written"
+    parser.add_argument("input1", metavar='<INPUT FASTQ1>',
+                        help="Input .fastq file. Assumes to contain read1 if given with second input file. If only "
+                             "input1 is given, input is assumed to be an interleaved fastq file. If reading from stdin"
+                             "is requested use '-' as a placeholder.")
+    parser.add_argument("input2", nargs='?', metavar='<INPUT FASTQ2>',
+                        help="Input .fastq file for read2 for paired-end read. Leave empty if using interleaved fastq.")
+    parser.add_argument("-o1", "--output1", default=None, metavar='<OUTPUT FASTQ1>',
+                        help="Output .fastq file name for read1. If not specified the result is written to stdout"
+                             " as interleaved fastq. If output1 given but not output2, output will be written as "
+                             "interleaved fastq to output1.")
+    parser.add_argument("-o2", "--output2", default=None, metavar='<OUTPUT FASTQ2>',
+                        help="Output .fastq file name for read2. If not specified but -o1 given the result is written"
                              " as interleaved fastq to o1.")
-    # Options
-    parser.add_argument("--interleaved", default=False, action='store_true',
-                        help="Input is interleaved. ")
