c9589e6e27d4bf3d2ea93195dbdab6f20dad784d
Pontus Hojer <pontushojer@gmail.com>
2019-07-04 11:05:49 +0200

    Refactored tagbam.py - Simplified and cleaned up code - Implemented tqdm for progress report

--- src/blr/cli/tagbam.py
+++ src/blr/cli/tagbam.py
@@ -4,8 +4,8 @@ Takes a fastq file barcode sequences in the header and writes a barcode fasta fi
 
 import pysam
 import logging
-
-import blr.utils as BLR
+import time
+from tqdm import tqdm
 
 logger = logging.getLogger(__name__)
 
@@ -13,64 +13,63 @@ logger = logging.getLogger(__name__)
 def main(args):
 
     # Generate dict with bc => bc_cluster consensus sequence
-    logger.info("Starting analysis")
-    clstr_generator = BLR.FileReader(args.input_clstr)
-    cluster_dict = process_clusters(clstr_generator.fileReader(), args.skip_nonclust)
-    clstr_generator.close()
+    logger.info(f'Starting analysis')
+
+    with open(args.input_clstr, "r") as clstr_file:
+        cluster_dict = process_clusters(clstr_file, skip_nonclust=args.skip_nonclust)
 
     # Read bam files and translate bc seq to BC cluster ID + write to out
-    progress = BLR.ProgressReporter('Reads processed', 1000000)
-    infile = pysam.AlignmentFile(args.input_mapped_bam, 'rb')
-    out = pysam.AlignmentFile(args.output_tagged_bam, 'wb', template=infile)
     reads_with_non_clustered_bc = int()
-    for read in infile.fetch(until_eof=True):
-        read_bc = read.query_name.split()[0].split('_')[-1]
+    with pysam.AlignmentFile(args.input_mapped_bam, 'rb') as infile, \
+            pysam.AlignmentFile(args.output_tagged_bam, 'wb', template=infile) as out:
 
-        # Fetch barcode cluster ID based on barcode sequence
-        if not read_bc in cluster_dict:
-            reads_with_non_clustered_bc += 1
-        else:
-            bc_id = cluster_dict[read_bc]
-            read.set_tag('BC', str(bc_id), value_type='Z')  # Stores as string, makes duplicate removal possible. Can do it as integer as well.
-            read.query_name = (read.query_name + '_BC:Z:' + str(bc_id))
+        for read in tqdm(infile.fetch(until_eof=True), desc="Reading .bam"):
+            read_bc = read.query_name.split()[0].split('_')[-1]
 
-        out.write(read)
-        progress.update()
+            # Fetch barcode cluster ID based on barcode sequence
+            if read_bc not in cluster_dict:
+                reads_with_non_clustered_bc += 1
+            else:
+                bc_id = cluster_dict[read_bc]
 
-    infile.close()
-    out.close()
-    logger.info('Finished')
+                # Stores as string, makes duplicate removal possible. Can do it as integer as well.
+                read.set_tag('BC', str(bc_id), value_type='Z')
+                read.query_name = f'{read.query_name}_BC:Z:{bc_id}'
+
+            out.write(read)
+
+    logger.info(f'Finished')
 
 
-def process_clusters(openInfile, skip_nonclust):
+def process_clusters(input_file, skip_nonclust=False):
     """
     Builds bc => bc_cluster dict (bc_cluster is given as the consensus sequence).
     """
 
     # For first loop
-    if skip_nonclust: seqs_in_cluster = 2
+    seqs_in_cluster = 2 if skip_nonclust else int()
 
     # Reads cluster file and saves as dict
     cluster_dict = dict()
-    cluster_ID = int()
-    for line in openInfile:
+    cluster_id = int()
+    for line in tqdm(input_file, desc="Reading .clstr"):
 
         # Reports cluster to master dict and start new cluster instance
-        if line.startswith('>'):
+        if line.startswith('>Cluster'):
 
             # If non-clustered sequences are to be omitted, removes if only one sequence makes out the cluster
             if skip_nonclust and seqs_in_cluster < 2:
                 del cluster_dict[current_key]
             seqs_in_cluster = 0
 
-            cluster_ID += 1
-            current_value = cluster_ID
+            cluster_id += 1
+            current_value = cluster_id
         else:
             current_key = line.split()[2].lstrip('>').rstrip('...').split(':')[2]
             cluster_dict[current_key] = current_value
-            seqs_in_cluster +=1
+            seqs_in_cluster += 1
 
-    return(cluster_dict)
+    return cluster_dict
 
 
 def add_arguments(parser):
