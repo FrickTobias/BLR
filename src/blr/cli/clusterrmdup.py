5ea5bc164b8b0704b7a3b0789ef458ad71eccb67
tobiasfrick <hans.tobias.frick@gmail.com>
2019-08-19 14:43:26 +0200

    Remove @ from bc in header.

--- src/blr/cli/clusterrmdup.py
+++ src/blr/cli/clusterrmdup.py
@@ -109,7 +109,7 @@ def main(args):
                 new_barcode_id = str(merge_dict[previous_barcode_id])
                 read.set_tag(args.barcode_tag, new_barcode_id, value_type="Z")
                 read.query_name = "_".join(
-                    read.query_name.split("_")[:-1]) + "_@" + args.barcode_tag + ":Z:" + new_barcode_id
+                    read.query_name.split("_", maxsplit=1)[:-1]) + "_" + args.barcode_tag + ":Z:" + new_barcode_id
 
                 # Merge file writing
                 if not new_barcode_id in bc_seq_already_written:
