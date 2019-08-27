93f33f1a71d8afe3761ca53ee82e7a2d4953192a
Pontus Hojer <pontushojer@gmail.com>
2019-08-27 09:49:33 +0200

    Test script updates:  - Updated md5sum after rebase  - Removed comment  - Changed to run on single core.

--- tests/run.sh
+++ tests/run.sh
@@ -3,14 +3,13 @@ set -xeuo pipefail
 
 ( cd testdata && bowtie2-build chr1mini.fasta chr1mini > /dev/null )
 
-# test single core execution
 rm -rf outdir
 mkdir -p outdir
 ln -s $PWD/testdata/reads.1.fastq.gz outdir/reads.1.fastq.gz
 ln -s $PWD/testdata/reads.2.fastq.gz outdir/reads.2.fastq.gz
 
 snakemake --configfile tests/test_config.yaml outdir/reads.1.final.fastq.gz \
-    outdir/reads.2.final.fastq.gz -j 3
+    outdir/reads.2.final.fastq.gz
 
 m=$(samtools sort -n outdir/mapped.sorted.tag.mkdup.bcmerge.filt.bam | samtools view - | md5sum | cut -f1 -d" ")
-test $m == faccb608e13c184c7ff0625c65dca78f
+test $m == 7f0d3e8bd8d5f147c1f4c4234b4e1efe
