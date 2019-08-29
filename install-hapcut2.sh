bf9d5eb3ba502a687bfd1c339132e077ec133c6e
Pontus Hojer <pontushojer@gmail.com>
2019-08-29 09:22:54 +0200

    Updated install-hapcut2.sh script so that a specific version 1.1 (commit 3cb79c1) is used. Added comment with link to github page.

--- install-hapcut2.sh
+++ install-hapcut2.sh
@@ -2,8 +2,13 @@
 
 set pipefail -eox
 
+# Based on https://github.com/vibansal/HapCUT2/releases.
+
 git clone https://github.com/vibansal/HapCUT2.git
 
 cd HapCUT2
+
+# For versions 1.1
+git checkout 3cb79c1
 
 make
