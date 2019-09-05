bda417115cce12cdc89f0f4d83d393a6e1962726
tobiasfrick <hans.tobias.frick@gmail.com>
2019-09-05 16:44:23 +0200

    Change which file name of final file

--- tests/run.sh
+++ tests/run.sh
@@ -11,5 +11,5 @@ ln -s $PWD/testdata/reads.2.fastq.gz outdir/reads.2.fastq.gz
 snakemake --configfile tests/test_config.yaml outdir/reads.1.final.fastq.gz \
     outdir/reads.2.final.fastq.gz
 
-m=$(samtools sort -n outdir/mapped.sorted.tag.mkdup.bcmerge.filt.bam | samtools view - | md5sum | cut -f1 -d" ")
+m=$(samtools sort -n outdir/mapped.sorted.tag.mols.mkdup.bcmerge.filt.bam | samtools view - | md5sum | cut -f1 -d" ")
 test $m == 7f0d3e8bd8d5f147c1f4c4234b4e1efe
