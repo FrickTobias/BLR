b37a48f72b3954f63a12802470769f12e16db95d
Marcel Martin <marcel.martin@scilifelab.se>
2019-09-16 17:56:30 +0200

    Use environment files for both abstract and locked dependencies
    With this change, we will have two types of Conda environment files:
    
    - The environment.yml file contains only abstract dependencies such as
      "picard=2.10" or "pysam".  This file is managed manually and needs to be
      updated whenever there are new dependencies or when the required version for
      dependencies change.
    
    - The environment.*.lock.yml contain a fully specified description of the
      entire environment (with locked-down versions).  This is needed because we
      want to have reproducible tests.  Use the script misc/condalock.sh to
      create these files whenever you make changes to environment.yml.

---
+++ misc/condalock.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# This script creates environment.(osx|linux).lock.yml from environment.yml.
+# It needs to be run manually whenever environment.yml is changed. See also
+# the developer documentation.
+
+set -euo pipefail
+
+if [[ -e ~/.condarc ]]; then
+    mv ~/.condarc ~/.condarc.condalock.bak
+    trap "mv ~/.condarc.condalock.bak ~/.condarc" EXIT
+else
+    trap "rm ~/.condarc" EXIT
+fi
+
+for os in linux osx; do
+    env=blrtmp-$RANDOM-$os
+    env_yml=environment.$os.lock.yml
+    printf "channels:\n  - conda-forge\n  - bioconda\n  - defaults\n" > ~/.condarc
+    printf "subdir: %s-64\nsubdirs:\n  - %s-64\n  - noarch\n" $os $os >> ~/.condarc
+    conda env create -n $env -f environment.yml
+    conda env export -n $env | grep -Ev '^(name|prefix):' > ${env_yml}
+    conda env remove -n $env
+    echo "Created ${env_yml}"
+done
+
+# Original .condarc will be restored by the exit trap
