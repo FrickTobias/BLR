d24c12ddee54fd0886de069fd286244d173c5dc7
Marcel Martin <marcel.martin@scilifelab.se>
2019-09-24 13:58:33 +0200

    Create a "blr init" command
    Close #94

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     },
     package_dir={"": "src"},
     packages=find_namespace_packages("src"),
-    package_data={"blr": ["Snakefile", "rules/*.smk", "config.schema.yaml"]},
+    package_data={"blr": ["Snakefile", "rules/*.smk", "config.schema.yaml", "config.yaml"]},
     entry_points={"console_scripts": ["blr = blr.__main__:main"]},
     classifiers=[
         "Development Status :: 4 - Beta",
