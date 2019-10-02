20b1a3f3b41bfa10396e15202bb1c8d29dcd8f5c
Pontus Hojer <pontushojer@gmail.com>
2019-10-02 14:52:42 +0200

    Updated test file to include starcode version and new md5sum.

--- tests/run.sh
+++ tests/run.sh
@@ -5,7 +5,7 @@ samtools --version
 bowtie2 --version
 picard SamToFastq --version || true
 cutadapt --version
-cd-hit --help | head -n 1 || true
+starcode --version
 snakemake --version
 blr --version
 
@@ -18,7 +18,7 @@ cd outdir
 blr run
 
 m=$(samtools view mapped.sorted.tag.mkdup.bcmerge.mol.filt.bam | md5sum | cut -f1 -d" ")
-test $m == 827612d1dd59d07071defa26bc8add4c
+test $m == e573dafc948e263a3112af2882bf0287
 
 # Test phasing
 blr run phasing_stats.txt
