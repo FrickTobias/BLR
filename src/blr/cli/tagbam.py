21cd8544944a18c375c52af44e550e06ae84734d
Pontus Hojer <pontushojer@gmail.com>
2019-10-04 17:06:38 +0200

    Updated tagbam based on comments in PR:  - Included handling of alignments missing barcodes in header which is now included from the changes in the tagfastq script.  - Other minor changes

--- src/blr/cli/tagbam.py
+++ src/blr/cli/tagbam.py
@@ -12,27 +12,38 @@ logger = logging.getLogger(__name__)
 def main(args):
 
     # Generate dict with bc => bc_cluster consensus sequence
-    logger.info(f'Starting analysis')
+    logger.info("Starting analysis")
+
+    alignments_missing_bc = 0
 
     # Read bam files and translate bc seq to BC cluster ID + write to out
     with pysam.AlignmentFile(args.input_mapped_bam, 'rb') as infile, \
             pysam.AlignmentFile(args.output_tagged_bam, 'wb', template=infile) as out:
 
-        for read in tqdm(infile.fetch(until_eof=True), desc="Reading .bam"):
-            read_header, cluster_seq, cluster_tag = read.query_name.split()[0].split('_')
-            cluster_id = cluster_tag.split(":")[-1]
+        for read in tqdm(infile.fetch(until_eof=True), desc="Reading BAM"):
+            try:
+                cluster_tag = read.query_name.split()[0].split('_')[-1]
+            except ValueError:
+                alignments_missing_bc += 1
+                cluster_tag = None
 
-            read.set_tag(args.barcode_cluster_tag, cluster_id, value_type='Z')
+            if cluster_tag:
+                cluster_id = cluster_tag.split(":")[-1]
+
+                read.set_tag(args.barcode_cluster_tag, cluster_id, value_type='Z')
+
             out.write(read)
 
-    logger.info(f'Finished')
+    logger.info(f"Alignments missing barcodes: {alignments_missing_bc}")
+    logger.info("Finished")
 
 
 def add_arguments(parser):
-    parser.add_argument("input_mapped_bam", metavar="<INPUT_BAM>",
-                        help=".bam file with mapped reads which is to be tagged with barcode id:s.")
-    parser.add_argument("output_tagged_bam",  metavar="<OUTPUT_BAM>",
-                        help=".bam file with barcode cluster id in the bc tag.")
-    parser.add_argument("-bc", "--barcode-cluster-tag", metavar="<STRING>", type=str, default="BX",
-                        help="Bam file tag where barcode cluster id is stored. 10x genomics longranger output "
-                             "uses 'BX' for their error corrected barcodes. DEFAULT: BX")
+    parser.add_argument("input_mapped_bam",
+                        help="BAM file with mapped reads which is to be tagged with barcode ids.")
+    parser.add_argument("output_tagged_bam",
+                        help="BAM file with barcode cluster id in the bc tag.")
+    parser.add_argument("--barcode-cluster-tag", "--bc", default="BX",
+                        help="BAM file tag where barcode cluster id is stored. 10x genomics "
+                             "longranger output uses 'BX' for their error corrected barcodes. "
+                             "Default: %(default)s")
