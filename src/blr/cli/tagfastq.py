54960d3f98b20443b44d5820f60d1f33f66ed487
tobiasfrick <hans.tobias.frick@gmail.com>
2019-11-04 11:28:47 +0100

    pep8: max len of row fix

--- src/blr/cli/tagfastq.py
+++ src/blr/cli/tagfastq.py
@@ -51,7 +51,8 @@ def main(args):
     separator = args.sep
     # Parse input FASTA/FASTQ for read1 and read2, raw barcodes and write output
     with dnaio.open(args.input1, file2=args.input2, interleaved=in_interleaved, mode="r", fileformat="fastq") as reader, \
-            dnaio.open(args.output1, file2=args.output2, interleaved=out_interleaved, mode="w", fileformat="fastq") as writer, \
+            dnaio.open(args.output1, file2=args.output2, interleaved=out_interleaved, mode="w",
+                       fileformat="fastq") as writer, \
             dnaio.open(args.raw_barcodes, mode="r") as raw_bc_reader:
 
         raw_bc_iterator = parse_raw_barcodes(raw_bc_reader)
